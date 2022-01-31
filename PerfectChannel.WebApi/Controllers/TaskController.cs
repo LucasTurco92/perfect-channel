4403771370c8be1c76f1ef9b640929d87d9cc5f2
lukitinchox@gmail.com <22123060+LucasTurco92@users.noreply.github.com>
2022-01-31 00:03:38 -0300

    my first commit

---
+++ PerfectChannel.WebApi/Controllers/TaskController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PerfectChannel.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(){
+            return "hello task";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# perfect-channel
+
+### `dotnet build`
+
+To build the entire solution.
+
+### `dotnet run -p .\PerfectChannel.WebApi\PerfectChannel.WebApi.csproj`
+
+Runs the app in the development mode.\
+Open [http://localhost:5000](http://localhost:5000) to view it in your browser.
+
+
